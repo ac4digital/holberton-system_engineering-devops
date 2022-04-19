727521624679ca6778009e36540f136839f575f4
Andrés Cataño <3866@holbertonschool.com>
2022-04-19 11:53:07 -0500

    Firts Commit

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,10 @@
 #Using Puppet, create a file in /tmp.
 
-file_line { '/tmp/school':
-    ensure => 'file',
-    owner => 'www-data',
-    mode => '0744',
-    group => 'www-data',
-    path => '/tmp/school',
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    mode    => '0744',
+    group   => 'www-data',
+    path    => '/tmp/school',
     content => 'I love Puppet',
 }
-
