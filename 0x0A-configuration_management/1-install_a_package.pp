f5fc90b1c3861abb29bb473d7eb5ce8643b8ee9f
Andrés Cataño <3866@holbertonschool.com>
2022-04-19 13:00:50 -0500

    Firts Commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,8 @@
 #Using Puppet, install puppet-lint 2.5.0.
 
 package { 'puppet-lint':
-    ensure     => '2.5.0',
-    provider   => 'gem',
-    name       => 'puppet-lint',
-    source     => 'https://rubygems.org/downloads/puppet-lint-2.5.0.gem',
+    ensure    => '2.5.0',
+    provider  => 'gem',
+    name      => 'puppet-lint',
+    source    => 'https://rubygems.org/downloads/puppet-lint-2.5.0.gem',
 }
