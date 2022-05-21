44dd71153cf3c28c11f7e1b50b2e601e356a4855
Andrés Cataño <3866@holbertonschool.com>
2022-05-21 11:49:13 -0500

    First Commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Write a function that requieres the Reddit API 
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Script that returns the number of subscribers for a given subreddit """
+    """if subreddit is None or type(subreddit) is not str:
+        return 0
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    header = {'User-Agent': 'Mozilla/5.0'}
+    r = requests.get(url, headers=header, allow_redirects=False).json()
+    try:
+        subs = r.get("data", {}).get("subscribers", 0)
+        return subs
+    except ValueError:
+        return 0
