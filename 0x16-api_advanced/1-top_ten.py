1f18ab4e97c98118daee04260440d2e926c4e565
Andrés Cataño <3866@holbertonschool.com>
2022-05-21 16:25:35 -0500

    First Commit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,12 +9,12 @@ import requests
 
 def top_ten(subreddit):
     """ Prints the titles of the top ten hot posts for a given subreddit """
-    
+
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     header = {'User-Agent': 'Mozilla/5.0'}
 
     r = requests.get(url, headers=header,
-                    allow_redirects=False).json()
+                     allow_redirects=False).json()
 
     try:
         list = r.get('data').get('children')
