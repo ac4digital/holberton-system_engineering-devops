5b6b5cdc873e2ae79cd408d8d83854e64bbadbdf
Andrés Cataño <3866@holbertonschool.com>
2022-05-21 16:13:35 -0500

    First Commit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddig API and prints the titles
+of the first 10 hot posts listed for a given subreddit
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """ Prints the titles of the top ten hot posts for a given subreddit """
+    
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    header = {'User-Agent': 'Mozilla/5.0'}
+
+    r = requests.get(url, headers=header,
+                    allow_redirects=False).json()
+
+    try:
+        list = r.get('data').get('children')
+        for i in list:
+            print(i.get('data').get('title'))
+    except ValueError:
+        print(None)
